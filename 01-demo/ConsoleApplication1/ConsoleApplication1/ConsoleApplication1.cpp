e8c6b0ec5aa2b16fe1425d7e4e9b37c0e6a8f570
MeowMeowZi <1649501854@qq.com>
2021-09-13 22:01:43 +0800

    01 demo

--- 01-demo/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ 01-demo/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,6 @@
 ﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
+#define MIN(x, y) (((x)>(y))?(y):(x))
 
 #include <iostream>
 #include <assert.h>
@@ -11,7 +12,10 @@ int main()
     int c = 0xfeel;
     std::cout << c << std::endl;*/
 
-    bool a = true, b = false;
+    /*bool a = true, b = false;
     assert(!(a && b) == (!a || !b));
-    assert(!(a || b) == (!a && !b));
+    assert(!(a || b) == (!a && !b));*/
+
+    /*int tmep = MIN(5, 6);
+    std::cout << tmep;*/
 }
