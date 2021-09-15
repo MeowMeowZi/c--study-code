4637610d8e8d259d61edb2ee0f5b594b9da84479
MeowMeowZi <1649501854@qq.com>
2021-09-15 18:38:50 +0800

    02 demo

---
+++ 02-demo/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,62 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	/*int a[] = { 1, 2, 3, 4 };
+	int len = sizeof(a) / sizeof(a[0]);
+
+	for (int i = 0; i < len; i++)
+	{
+		cout << a[i] << " ";
+	}
+
+	cout << endl;*/
+
+	/*vector<int> vec = { 1, 2, 3, 4 };
+	
+	cout << "size is " << vec.size() << endl;
+	cout << "capacity is " << vec.capacity() << endl;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << endl;
+	}
+	
+	vec.push_back(6);
+	cout << "size is " << vec.size() << endl;
+	cout << "capacity is " << vec.capacity() << endl;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << endl;
+	}
+	
+	vec.insert(--vec.end(), 7);
+	cout << "size is " << vec.size() << endl;
+	cout << "capacity is " << vec.capacity() << endl;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << endl;
+	}
+	
+	vec.pop_back();
+	cout << "size is " << vec.size() << endl;
+	cout << "capacity is " << vec.capacity() << endl;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << endl;
+	}
+
+	vec.erase(vec.end() - 2)
+	cout << "size is " << vec.size() << endl;
+	cout << "capacity is " << vec.capacity() << endl;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << endl;
+	}*/
+
+
+}
